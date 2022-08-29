6053aea774e8f08586a8a026b54b7996c306b58e
Tanguy Dietrich <tanguydietrich@gmail.com>
2022-08-30 00:22:38 +0200

    Implemented all common method to ASCOM device

--- ascom/server/Telescope.py
+++ ascom/server/Telescope.py
@@ -12,6 +12,7 @@ from fastapi_utils.inferring_router import InferringRouter
 from pydantic import BaseModel
 from starlette.requests import Request
 from aiocache import Cache
+import random
 
 
 API_NAME = "api"
@@ -27,12 +28,23 @@ router = InferringRouter()
 
 @cbv(router)
 class Telescope:
-    def __init__(self, ip="127.0.0.1", port=32323, deviceNumber=0):
+    def __init__(self, ip="127.0.0.1", port=32323, deviceNumber=0, name="Astrophotography Assistant", descriptions="blabla", driverinfo="", driverversion="", interfaceversion="", supportedactions=[""]):
+        # WARNING EVERYTHING HERE IS READ ONLY AFTER RUN
         self.ip = ip
         self.deviceNumber = deviceNumber
         self.port = port
+        self.name = name
+        self.descriptions = descriptions
+        self.driverinfo = driverinfo
+        self.driverversion = driverversion
+        self.interfaceversion = interfaceversion
+        self.supportedactions = supportedactions
         # TODO FIX THIS -> deviceNumber not the good value
         #DEFAULT_LINK = "/"+API_NAME+"/"+API_VERSION+"/"+DEVICE_TYPE+"/"+str(deviceNumber)+"/"
+
+    def returnValue(self, Value, ClientTransactionID, ErrorNumber=0, ErrorMessage=""):
+        ServerTransID = random.randint(0,4294967295)
+        return {"Value": Value, "ClientTransactionID": ClientTransactionID, "ServerTransactionID":  ServerTransID, "ErrorNumber": ErrorNumber, "ErrorMessage": ErrorMessage}
 
     def run(self):
         uvicorn.run(app, host=self.ip, port=self.port)
@@ -46,15 +58,63 @@ class Telescope:
 ##                      COMMON FUNCTION
 ##
 ################################################################################
-    @router.get(DEFAULT_LINK+"connected")
-    async def Getconnected(self, ClientTransactionID: int, ClientID: int):
-        co = await cache.get("Connected", default=False)
-        return {"Value": co, "ErrorNumber": 0, "ErrorMessage": ""}
 
+
+##################################     SET    ##################################
     @router.put(DEFAULT_LINK+"connected")
     async def setConnected(self, Connected: bool = Form(...), ClientID: int = Form(...), ClientTransactionID: int = Form(...)):
         await cache.set("Connected", Connected)
-        return {"ErrorNumber": 0, "ErrorMessage": ""}
+        return self.returnValue("", ClientTransactionID)
+
+    @router.put(DEFAULT_LINK+"action")
+    async def setAction(self, Action: str = Form(...), Parameters: str = Form(...), ClientID: int = Form(...), ClientTransactionID: int = Form(...)):
+        #TODO : SEE WHAT I HAVE TO DO HERE...
+        return self.returnValue("", ClientTransactionID)
+
+    @router.put(DEFAULT_LINK+"commandblind")
+    async def setCommandblind(self, Command: str = Form(...), Raw: str = Form(...), ClientID: int = Form(...), ClientTransactionID: int = Form(...)):
+        #TODO : SEE WHAT I HAVE TO DO HERE...
+        return self.returnValue("", ClientTransactionID)
+
+    @router.put(DEFAULT_LINK+"commandbool")
+    async def setCommandbool(self, Command: str = Form(...), Raw: str = Form(...), ClientID: int = Form(...), ClientTransactionID: int = Form(...)):
+        #TODO : SEE WHAT I HAVE TO DO HERE...
+        return self.returnValue(False, ClientTransactionID)
+
+    @router.put(DEFAULT_LINK+"commandstring")
+    async def setCommandstring(self, Command: str = Form(...), Raw: str = Form(...), ClientID: int = Form(...), ClientTransactionID: int = Form(...)):
+        #TODO : SEE WHAT I HAVE TO DO HERE...
+        return self.returnValue("", ClientTransactionID)
+
+##################################     GET    ##################################
+    @router.get(DEFAULT_LINK+"connected")
+    async def Getconnected(self, ClientTransactionID: int, ClientID: int):
+        co = await cache.get("Connected", default=False)
+        return self.returnValue(co, ClientTransactionID)
+
+    @router.get(DEFAULT_LINK+"name")
+    async def Getname(self, ClientTransactionID: int, ClientID: int):
+        return self.returnValue(self.name, ClientTransactionID)
+
+    @router.get(DEFAULT_LINK+"description")
+    async def Getdescriptions(self, ClientTransactionID: int, ClientID: int):
+        return self.returnValue(self.descriptions, ClientTransactionID)
+
+    @router.get(DEFAULT_LINK+"driverinfo")
+    async def Getdriverinfo(self, ClientTransactionID: int, ClientID: int):
+        return self.returnValue(self.driverinfo, ClientTransactionID)
+
+    @router.get(DEFAULT_LINK+"driverversion")
+    async def Getdriverversion(self, ClientTransactionID: int, ClientID: int):
+        return self.returnValue(self.driverversion, ClientTransactionID)
+
+    @router.get(DEFAULT_LINK+"interfaceversion")
+    async def Getinterfaceversion(self, ClientTransactionID: int, ClientID: int):
+        return self.returnValue(self.interfaceversion, ClientTransactionID)
+
+    @router.get(DEFAULT_LINK+"supportedactions")
+    async def Getsupportedactions(self, ClientTransactionID: int, ClientID: int):
+        return self.returnValue(self.supportedactions, ClientTransactionID)
 
 ################################################################################
 ##
--- ascom/server/main.py
+++ ascom/server/main.py
@@ -1,4 +1,6 @@
 import Telescope as T
 
+# t = T.Telescope(descriptions="Technician diploma project")
 t = T.Telescope()
+
 t.run()
