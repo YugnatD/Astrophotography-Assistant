e5a87b3bef0bf16752b4c92952fede9ef74bf457
Tanguy Dietrich <tanguydietrich@gmail.com>
2022-08-29 23:35:47 +0200

    Working example for Connected

--- ascom/server/Telescope.py
+++ ascom/server/Telescope.py
@@ -11,6 +11,8 @@ from fastapi_utils.cbv import cbv
 from fastapi_utils.inferring_router import InferringRouter
 from pydantic import BaseModel
 from starlette.requests import Request
+from aiocache import Cache
+
 
 API_NAME = "api"
 API_VERSION = "v1"
@@ -18,23 +20,22 @@ DEVICE_TYPE = "telescope"
 DEVICE_NUMBER = "0"
 DEFAULT_LINK = "/"+API_NAME+"/"+API_VERSION+"/"+DEVICE_TYPE+"/"+DEVICE_NUMBER+"/"
 
+cache = Cache()
 app = FastAPI()
 router = InferringRouter()
+
 
 @cbv(router)
 class Telescope:
     def __init__(self, ip="127.0.0.1", port=32323, deviceNumber=0):
-        app.ip = ip
-        app.deviceNumber = deviceNumber
-        app.port = port
-        app.Connected = False
-        app.ClientID = 0
-        app.ClientTransactionID = 0
+        self.ip = ip
+        self.deviceNumber = deviceNumber
+        self.port = port
         # TODO FIX THIS -> deviceNumber not the good value
         #DEFAULT_LINK = "/"+API_NAME+"/"+API_VERSION+"/"+DEVICE_TYPE+"/"+str(deviceNumber)+"/"
 
     def run(self):
-        uvicorn.run(app, host=app.ip, port=app.port)
+        uvicorn.run(app, host=self.ip, port=self.port)
 
     @router.get("/")
     def index(self):
@@ -46,16 +47,13 @@ class Telescope:
 ##
 ################################################################################
     @router.get(DEFAULT_LINK+"connected")
-    def Getconnected(self, ClientTransactionID: int, ClientID: int):
-        print("connected state "+str(app.Connected)+ " "+str(ClientTransactionID))
-        return {"Value": app.Connected, "ErrorNumber": 0, "ErrorMessage": ""}
+    async def Getconnected(self, ClientTransactionID: int, ClientID: int):
+        co = await cache.get("Connected", default=False)
+        return {"Value": co, "ErrorNumber": 0, "ErrorMessage": ""}
 
     @router.put(DEFAULT_LINK+"connected")
-    def setConnected(self, Connected: bool = Form(...), ClientID: int = Form(...), ClientTransactionID: int = Form(...)):
-        app.Connected = Connected
-        app.ClientID = ClientID
-        app.ClientTransactionID = ClientTransactionID
-        print("setting connected to : "+str(app.Connected))
+    async def setConnected(self, Connected: bool = Form(...), ClientID: int = Form(...), ClientTransactionID: int = Form(...)):
+        await cache.set("Connected", Connected)
         return {"ErrorNumber": 0, "ErrorMessage": ""}
 
 ################################################################################
